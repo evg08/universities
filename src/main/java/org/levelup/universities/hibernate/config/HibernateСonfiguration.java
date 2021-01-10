167943f546b3d7e33309c2e72dec335fb09b1cb9
ekhozova <Evgeniia.Khozova@t-systems.com>
2021-01-10 20:21:48 +0300

    Hibernate settings via file was removed and replaced by java

--- src/main/java/org/levelup/universities/hibernate/config/HibernateСonfiguration.java
+++ src/main/java/org/levelup/universities/hibernate/config/HibernateСonfiguration.java
@@ -1,7 +1,13 @@
 package org.levelup.universities.hibernate.config;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.levelup.universities.hibernate.domain.*;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.util.Properties;
 
 public final class HibernateСonfiguration {
     private HibernateСonfiguration() {
@@ -10,9 +16,40 @@ public final class HibernateСonfiguration {
     private static SessionFactory factory = initializeSessionFactory();
 
     private static SessionFactory initializeSessionFactory() {
-        Configuration configuration =new Configuration()
-                .configure();
-        return configuration.buildSessionFactory();
+      //  Configuration configuration =new Configuration()
+        //        .configure();
+        if (factory==null){
+        try {
+
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "org.postgresql.Driver");
+            settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/universities_2");
+            settings.put(Environment.USER, "postgres");
+            settings.put(Environment.PASS, "admin");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL10Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.FORMAT_SQL, "true");
+            settings.put(Environment.HBM2DDL_AUTO, "validate");
+
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(UniversityEntity.class);
+            configuration.addAnnotatedClass(FacultyEntity.class);
+            configuration.addAnnotatedClass(SubjectEntity.class);
+            configuration.addAnnotatedClass(SubjectInfoEntity.class);
+            configuration.addAnnotatedClass(DepartmentEntity.class);
+            configuration.addAnnotatedClass(StudentGroupEntity.class);
+            configuration.addAnnotatedClass(StudentEntity.class);
+
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+            factory=configuration.buildSessionFactory(builder.build());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        }
+         return factory;
+      //  return configuration.buildSessionFactory();
     }
 
     //служит для получения объекта Session Factory
